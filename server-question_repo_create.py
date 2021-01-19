6313e73df5f950aead9294e837d2af98ebb43883
Swallodream <1297271665@qq.com>
2021-01-19 17:15:00 +0800

    2021/1/19

--- server-question_repo_create.py
+++ server-question_repo_create.py
@@ -1,1 +1,41 @@
 #本文件用来生成随机题库，题库根据难度选择生成
+
+#在题库保有函数中，构造了单选题库和多选题库，以及题目总数
+#根据这些信息生成针对个人的随机题库
+
+import random
+#第一个序号用于生成题目序号的随机数列
+def question_repo_create(tables,tables_quantity,repo_quantity):
+    repo_number_list = random.sample(range(1, tables_quantity), repo_quantity)  
+    return repo_number_list
+
+#利用生成的序号数组，从题库中选出题目
+def repo_create(tables,repo_number_list):
+    repo = []
+    for i in repo_number_list:
+        repo.append(tables[i])
+    
+    return repo
+
+#利用上述函数生成单选题库
+def single_repo_create(single_tables,single_tables_number,single_repo_num=6):
+    #生成单选题库的题库序号
+    single_repo_number_list = question_repo_create(single_tables,single_tables_number,single_repo_num)
+    #根据题库序号生成题库
+    single_repo = repo_create(single_tables,single_repo_number_list)
+
+    return single_repo
+
+#利用上述函数生成多选题库
+def multi_repo_create(multi_tables,multi_tables_number,multi_repo_num=4):
+    #生成多选题库的题库序号
+    multi_repo_number_list = question_repo_create(multi_tables,multi_tables_number,multi_repo_num)
+    #根据题库序号生成题库
+    multi_repo = repo_create(multi_tables,multi_repo_number_list)
+
+    return multi_repo
+    
+    
+
+    
+    
--- server-questions-save.py
+++ server-questions-save.py
@@ -2,20 +2,32 @@
 #题库的载入格式是excel格式
 
 import xlrd
-data = xlrd.open_workbook(r'question_excel.xls')
-table = data.sheets()[0]
 
-tables=[]
 
-#信息载入函数，用来导入excel中的题目信息，导入的内容包括题目类型、题目主干、题目选项、题目答案、题目难度
-def import_excel(excel):
-    for rown in range(excel.nrows):
+
+
+#信息载入函数，用来导入excel中的题目信息，导入的内容包括题目类型、题目主干、题目选项、题目答案、题目难度;这里同样需要得到总行数，以计算题目的总数；
+#注意单选和多选需要分开储存；
+
+def import_excel(excel_file):
+    data = xlrd.open_workbook(excel_file)
+    table = data.sheet_by_index(0)
+    single_tables=[]
+    single_number=0
+    multi_tables=[]
+    multi_number=0
+    for rown in range(table.nrows):
         array = {'question_type':'','question':'','question_choice':'','question_answer':'','question_diffculty':''}
         array['question_type'] = table.cell_value(rown,0)
         array['question'] = table.cell_value(rown,1)
         array['question_choice'] = table.cell_value(rown,2)
         array['question_answer'] = table.cell_value(rown,3)
         array['question_diffculty'] = table.cell_value(rown,5)
-    
-    tables.append(array)
+        if table.cell_value(rown,0) == '单选题':
+            single_tables.append(array)
+            single_number++
+        if table.cell_value(rown,0) == '多选题':
+            multi_tables.append(array)
+            multi_number++
+    return single_tables,multi_tables,single_number,multi_number
 

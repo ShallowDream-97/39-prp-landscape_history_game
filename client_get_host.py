41c50a8089d4f40d2751b8a6ed046a04fe494dc3
sleepbefore11 <76658457+sleepbefore11@users.noreply.github.com>
2021-02-15 15:53:49 +0800

    Update client_get_host.py
    没看懂，不过我知道这是用来获取用户IP地址的模块，里面包含了一个获取用户IP的函数

--- client_get_host.py
+++ client_get_host.py
@@ -1,10 +1,10 @@
 import socket
 
 #the function used for client to get self_host
-def get_host_ip(): 
+def get_host_ip():    #获取主机ip
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #创建套接字对象s
+        s.connect(('8.8.8.8', 80))   #主动初始化TCP服务器连接，。一般address的格式为元组（hostname,port），如果连接出错，返回socket.error错误。
         ip = s.getsockname()[0]
     finally:
         s.close()
